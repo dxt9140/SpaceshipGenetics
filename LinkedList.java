0a30bacc4d9f27e6a09df9bb3cc5a419a38d08d4
dxt9140 <dxt9140@g.rit.edu>
2018-03-05 19:27:35 -0500

    LinkedList is seemingly complete enough for me to move on. Removed all error messages from compilation. NO documentation completed

--- LinkedList.java
+++ LinkedList.java
@@ -11,9 +11,9 @@ public class LinkedList<E> {
 
     private static class Node<E> {
 
-        private Node prev;
+        private Node<E> prev;
         private E val;
-        private Node next;
+        private Node<E> next;
 
         private Node( E val ) {
             this.val = val;
@@ -21,19 +21,19 @@ public class LinkedList<E> {
             this.next = null;
         }
 
-        public Node getNext() {
+        public Node<E> getNext() {
             return this.next;
         }
 
-        public Node getPrev() {
+        public Node<E> getPrev() {
             return this.prev;
         }
 
-        public void setNext( Node next ) {
+        public void setNext( Node<E> next ) {
             this.next = next;
         }
 
-        public void setPrev( Node prev ) {
+        public void setPrev( Node<E> prev ) {
             this.prev = prev;
         }
 
@@ -48,32 +48,39 @@ public class LinkedList<E> {
     public void append( E val ) {
         Node<E> node = new Node<E>( val );
 
-        if ( this.root == null ) {
+        if ( this.getRoot() == null ) {
+
             this.root = node;
             this.tail = node;
-            node.setPrev( null );
-            node.setNext( null );
+
         } else {
-            Node consider = this.root;
+
+            Node<E> consider = this.getRoot();
             while( consider.getNext() != null ) {
                 consider = consider.getNext();
             }
 
             consider.setNext( node );
-            node.setNext( null );
             node.setPrev( consider );
+ 
+            this.tail = node;
         }
         
     }
 
     public void insert( E val, int index ) {
+        if ( index == 0 ) {
+            this.prepend( val );
+            return;
+        }
+
         Node<E> node = new Node<E>( val );
-        Node consider = this.root;
+        Node<E> consider = this.getRoot();
         int hops = 0;
 
         try {
 
-            while ( consider != null && hops != index ) {
+            while ( hops != index ) {
                 if ( consider == null ) {
                     throw new IndexOutOfBoundsException();
                 }
@@ -82,9 +89,10 @@ public class LinkedList<E> {
                 hops++;
             }
 
-            Node temp = consider.getPrev();
-            node.setPrev( temp );
+            Node<E> temp = consider.getPrev();
+            temp.setNext( node );
             consider.setPrev( node );
+            node.setPrev( temp );
             node.setNext( consider );
 
             while ( this.root.getPrev() != null ) {
@@ -95,14 +103,77 @@ public class LinkedList<E> {
             }
 
         } catch ( IndexOutOfBoundsException exc ) {
-            System.err.println("Index out of bounds.");
+            System.err.println("Error: Error adding " + val + " to list.");
+            System.err.println("Error: Index " + index + " out of bounds.");
             return;
         }
 
     }
 
-    public Node getRoot() {
+    public void prepend( E val ) {
+        Node<E> node = new Node<E>( val );
+        Node<E> root = this.getRoot();
+        node.setNext( root );
+        root.setPrev( node );
+
+        this.root = node;
+    }
+
+    public E remove( int index ) {
+
+        try {
+
+            if ( index == 0 ) {
+                Node<E> node = this.getRoot();
+                this.root = node.getNext();
+                this.root.setPrev( null );
+                return node.val;
+            }
+
+            Node<E> consider = this.getRoot();
+            int hops = 0;
+            while ( hops != index ) {
+                if ( consider == null ) {
+                    throw new IndexOutOfBoundsException();
+                }
+                consider = consider.getNext();
+                hops++;
+            }
+
+            Node<E> node = consider;
+            Node<E> temp = consider.getPrev();
+            temp.setNext( consider.getNext() );
+            consider.getNext().setPrev( temp );
+
+            while ( this.root.getPrev() != null ) {
+                this.root = this.root.getPrev();
+            }
+            while ( this.tail.getNext() != null ) {
+                this.tail = this.tail.getNext();
+            }
+
+            return node.val;
+
+        } catch ( IndexOutOfBoundsException exc ) {
+            System.err.println("Error: Error removing element at index " + 
+                index + ".");
+            System.err.println("Error: Index " + index + " out of bounds.");
+            return null;
+        }
+
+    }
+
+    public Node<E> getRoot() {
         return this.root;
+    }
+
+    public void printLinkedList() {
+        Node<E> n = this.getRoot();
+        while ( n != null ) {
+            System.out.print( n.val + " " );
+            n = n.next;
+        }
+        System.out.println();
     }
 
     public static void main( String[] args ) {
@@ -110,15 +181,35 @@ public class LinkedList<E> {
         LinkedList<Integer> intList = new LinkedList<Integer>();
         
         intList.append( 8 );
-        intList.append( 7 );
-        intList.insert( 4, 1 );
+        intList.printLinkedList();
 
-        Node n = intList.getRoot();
-        while ( n != null ) {
-            System.out.print( n.val + " " );
-            n = n.next;
-        }
-        System.out.println();
+        intList.append( 7 );
+        intList.printLinkedList();
+
+        intList.insert( 4, 1 );
+        intList.printLinkedList();
+
+        intList.insert( 5, 0 );
+        intList.printLinkedList();
+
+        intList.insert( 99, 99 );
+        intList.printLinkedList();
+
+        intList.prepend( 1 );
+        intList.printLinkedList();
+
+        Integer removed = intList.remove( 1 );
+        intList.printLinkedList();
+        System.out.println( "("+ removed + ")" );
+
+        Integer removed_2 = intList.remove( 0 );
+        intList.printLinkedList();
+        System.out.println( "(" + removed_2 + ")" );
+
+        Integer removed_3 = intList.remove( 99 );
+        intList.printLinkedList();
+        System.out.println( "(" + removed_3 + ")" );
+
     }
 
 }
